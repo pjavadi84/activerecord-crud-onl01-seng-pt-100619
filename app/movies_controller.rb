850e1dca034da4633fcceecf6fabe66fb216c385
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-07 22:40:08 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -7,7 +7,8 @@ require 'pry'
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = Movie.new(title)
+  title = {"This is a title"}
+  movie = Movie.new(:title)
   movie.save
   movie
 end
