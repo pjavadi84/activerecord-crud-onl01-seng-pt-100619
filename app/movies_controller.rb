4676f04bc57fc516486ddfc184a0624716c22673
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-10 03:22:49 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -95,8 +95,8 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  movie = Movie.find_by(title: "")
-  __
+  movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
+  movie.d
 end
 
 def can_destroy_all_items_at_once
