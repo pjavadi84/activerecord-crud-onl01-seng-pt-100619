8176ba03f49b1f92ff7ec39dd8f4309e1f5f4a11
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-10 03:11:41 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -88,8 +88,8 @@ def can_update_multiple_items_at_once
   5.times do |i|
     movie = Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  movie.all.each do |updated_title|
-    
+  movie.all.each do |title|
+    title.update
   end
 end
 
