4de89f71543cec5efa9088d779badf602f411765
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-10 03:05:26 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -72,7 +72,7 @@ end
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   movie = Movie.create(title: "Awesome Flick")
-  movie.find_by(title: "Awesome Flick")
+  movie = Movie.find_by(title: "Awesome Flick")
   movie.update(title: "Even Awesomer Flick" )
   movie.save
 end
@@ -80,7 +80,7 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   movie = Movie.create(title: "Wat?")
-  movie.update(title:"Wat, huh?")
+  movie = Movie.update(title:"Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
